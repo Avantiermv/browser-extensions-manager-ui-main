bf828b2aedf41c33d4b3ab7d0cf93d37087e1698
Marcos Vinicius <avantiermv@gmail.com>
2025-07-25 14:14:44 -0300

    .

--- assets/script/main.js
+++ assets/script/main.js
@@ -21,6 +21,73 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     takeExtension('assets/script/data.json');
 
+    function createElements(extension){
+        const extensionDiv = document.createElement('div');
+        extensionDiv.classList.add('extension');
+        extensionDiv.id = `divExtension`;
+
+        const extensionContent = document.createElement('div');
+        extensionContent.classList.add('extension-content');
+
+        const divFavicon = document.createElement('div');
+        divFavicon.classList.add('extension-favicon');
+        const faviconImg = document.createElement('img');
+        faviconImg.src = extension.logo;
+        divFavicon.appendChild(faviconImg);
+
+        const divText = document.createElement('div');
+        divText.classList.add('extension-text');
+        const divh2 = document.createElement('h2');
+        divh2.innerText = extension.name;
+        divh2.classList.add('extension-title');
+        const divp = document.createElement('p');
+        divp.innerText = extension.description;
+        divp.classList.add('extension-info');
+        divText.appendChild(divh2);
+        divText.appendChild(divp);
+
+        extensionContent.appendChild(divFavicon);
+        extensionContent.appendChild(divText);
+
+        const extensionActions = document.createElement('div');
+        extensionActions.classList.add('extension-actions');
+
+        const buttonRemove = document.createElement('button');
+        buttonRemove.classList.add('extension-remove', 'toggles');
+        buttonRemove.innerText = "Remove";
+        buttonRemove.addEventListener('click', () => {
+            remove(extensionDiv);
+        })
+
+        const labelSwitch = document.createElement('label');
+        labelSwitch.classList.add('switch');
+        const inputCheckbox = document.createElement('input');
+        inputCheckbox.type = 'checkbox';
+        inputCheckbox.id = `inactiveActive-${extension.name}`;
+
+        if(extension.isActive === true){
+            inputCheckbox.checked = true;
+        }
+
+        inputCheckbox.addEventListener('click', () => {
+            trackSwitch(extension);
+        });
+
+        const spanSlider = document.createElement('span');
+        spanSlider.classList.add('slider', 'round');
+        labelSwitch.appendChild(inputCheckbox);
+        labelSwitch.appendChild(spanSlider);
+
+        extensionActions.appendChild(buttonRemove);
+        extensionActions.appendChild(labelSwitch);
+
+
+        extensionDiv.appendChild(extensionContent);
+        extensionDiv.appendChild(extensionActions);
+
+        extensionsdiv.appendChild(extensionDiv);
+    }
+
     function trackSwitch(extension){
         if(!extension.isActive){
             console.log("Não estava ligado mas agora está");
@@ -58,66 +125,11 @@ document.addEventListener('DOMContentLoaded', () => {
             e.classList.add('colors');
         });
     }
-    
-    function createElements(extension){
-        const extensionDiv = document.createElement('div');
-        extensionDiv.classList.add('extension');
 
-        const extensionContent = document.createElement('div');
-        extensionContent.classList.add('extension-content');
-
-        const divFavicon = document.createElement('div');
-        divFavicon.classList.add('extension-favicon');
-        const faviconImg = document.createElement('img');
-        faviconImg.src = extension.logo;
-        divFavicon.appendChild(faviconImg);
-
-        const divText = document.createElement('div');
-        divText.classList.add('extension-text');
-        const divh2 = document.createElement('h2');
-        divh2.innerText = extension.name;
-        divh2.classList.add('extension-title');
-        const divp = document.createElement('p');
-        divp.innerText = extension.description;
-        divp.classList.add('extension-info');
-        divText.appendChild(divh2);
-        divText.appendChild(divp);
-
-        extensionContent.appendChild(divFavicon);
-        extensionContent.appendChild(divText);
-
-        const extensionActions = document.createElement('div');
-        extensionActions.classList.add('extension-actions');
-
-        const buttonRemove = document.createElement('button');
-        buttonRemove.classList.add('extension-remove', 'toggles');
-        buttonRemove.innerText = "Remove";
-        const labelSwitch = document.createElement('label');
-        labelSwitch.classList.add('switch');
-        const inputCheckbox = document.createElement('input');
-        inputCheckbox.type = 'checkbox';
-        inputCheckbox.id = `inactiveActive-${extension.name}`;
-
-        if(extension.isActive === true){
-            inputCheckbox.checked = true;
-        }
-
-        inputCheckbox.addEventListener('click', () => {
-            trackSwitch(extension);
-        });
-
-        const spanSlider = document.createElement('span');
-        spanSlider.classList.add('slider', 'round');
-        labelSwitch.appendChild(inputCheckbox);
-        labelSwitch.appendChild(spanSlider);
-
-        extensionActions.appendChild(buttonRemove);
-        extensionActions.appendChild(labelSwitch);
-
-
-        extensionDiv.appendChild(extensionContent);
-        extensionDiv.appendChild(extensionActions);
-
-        extensionsdiv.appendChild(extensionDiv);
+    function remove(extension){
+        extension.style.display = 'none';
     }
+
+
+
 });

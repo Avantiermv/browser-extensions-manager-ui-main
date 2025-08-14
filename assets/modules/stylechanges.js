7957c999e3587b7423beeac943543e0d997cd84c
Marcos Vinicius <avantiermv@gmail.com>
2025-08-14 20:21:35 -0300

    :lipstick: feat: Change Mode Added
    I did some changes in the script to change the mode of the app to dark or light. Initially, the app will start in the dark mode.

--- assets/modules/stylechanges.js
+++ assets/modules/stylechanges.js
@@ -4,53 +4,158 @@ const switchDarkLight = document.querySelector('.switch-dark-light');
 const logoLight = document.querySelector('.svg-logo-image');
 const iconOfSwitch = document.querySelector('.image-sun');
 const switchDarkLightInput = document.querySelector('.switch-dark-light-input');
+const switchDarkLightLabel = document.querySelector('.switch-dark-light');
 const extensionListTitle = document.querySelector('.extension-list-title');
 const toggles = document.querySelectorAll('.identifier');
 
+const elements = {
+    cards: document.querySelectorAll('.card'),
+    extensionName: document.querySelectorAll('.extension-name'),
+    extensionDescription: document.querySelectorAll('.extension-description'),
+    extensionRemoveButtons: document.querySelectorAll('.extension-remove')
+}
+
+function toggleClasses(elements, classToAdd, classToRemove){
+    elements.forEach(el => {
+        el.classList.add(classToAdd);
+        el.classList.remove(classToRemove);
+    })
+}
+
+const themes = {
+    dark: {
+        add: {
+            body: 'background-dark',
+            header: 'header-dark',
+            switch: 'switch-dark-light-when-dark',
+            title: 'h1-dark',
+            toggles: 'identifier-dark',
+            cards: 'card-dark',
+            names: 'h2-dark',
+            descriptions: 'p-dark',
+            removeButtons: 'button-remove-dark', 
+        },
+        remove: {
+            body: 'background-light',
+            header: 'header-light',
+            switch: 'switch-dark-light-when-light',
+            title: 'h1-light',
+            toggles: 'identifier-light',
+            cards: 'card-light',
+            names: 'h2-light',
+            descriptions: 'p-light',
+            removeButtons: 'button-remove-light'
+        }
+    },
+    light:{
+        add: {
+            body: 'background-light',
+            header: 'header-light',
+            switch: 'switch-dark-light-when-light',
+            title: 'h1-light',
+            toggles: 'identifier-light',
+            cards: 'card-light',
+            names: 'h2-light',
+            descriptions: 'p-light',
+            removeButtons: 'button-remove-light'
+        },
+        remove: {
+            body: 'background-dark',
+            header: 'header-dark',
+            switch: 'switch-dark-light-when-dark',
+            title: 'h1-dark',
+            toggles: 'identifier-dark',
+            cards: 'card-dark',
+            names: 'h2-dark',
+            descriptions: 'p-dark',
+            removeButtons: 'button-remove-dark'
+        }
+    }
+}
+
+function updateDynamicElements() {
+    elements.cards = document.querySelectorAll('.card');
+    elements.extensionName = document.querySelectorAll('.extension-name');
+    elements.extensionDescription = document.querySelectorAll('.extension-description');
+    elements.extensionRemoveButtons = document.querySelectorAll('.extension-remove');
+}
+
+
 function changeToDark(){
-    body.classList.add('background-dark');
-    header.classList.add('header-dark');
-    switchDarkLight.classList.add('switch-dark-light-when-dark');
-    extensionListTitle.classList.add('h1-dark');
-    toggles.forEach( el => {
-        el.classList.add('identifier-dark');
-    });
-    document.querySelectorAll('.card').forEach( el => {
-        el.classList.add('card-dark');
-    });
-    document.querySelectorAll('.extension-name').forEach( el => {
-        el.classList.add('h2-dark')
-    });
-    document.querySelectorAll('.extension-description').forEach( el => {
-        el.classList.add('p-dark');
-    });
-    document.querySelectorAll('.extension-remove').forEach( el => {
-        el.classList.add('button-remove-dark');
-    });
+    updateDynamicElements();
+
+    switchDarkLightInput.checked = true;
+    logoLight.classList.remove('svg-dark');
+    body.classList.add(themes.dark.add.body);
+    header.classList.add(themes.dark.add.header);
+    switchDarkLight.classList.add(themes.dark.add.switch);
+    extensionListTitle.classList.add(themes.dark.add.title);
+
+    body.classList.remove(themes.dark.remove.body);
+    header.classList.remove(themes.dark.remove.header);
+    switchDarkLight.classList.remove(themes.dark.remove.switch);
+    extensionListTitle.classList.remove(themes.dark.remove.title);
+
+    toggleClasses(toggles, themes.dark.add.toggles, themes.dark.remove.toggles);
+    toggleClasses(elements.cards, themes.dark.add.cards, themes.dark.remove.cards);
+    toggleClasses(elements.extensionName, themes.dark.add.names, themes.dark.remove.names);
+    toggleClasses(elements.extensionDescription, themes.dark.add.descriptions, themes.dark.remove.descriptions);
+    toggleClasses(elements.extensionRemoveButtons, themes.dark.add.removeButtons, themes.dark.remove.removeButtons);
+
+    iconOfSwitch.src = 'assets/images/icon-sun.svg';
 }
 
 function changeToLight(){
-    body.classList.add('background-light');
-    header.classList.add('header-light');
+    updateDynamicElements();
+
+    switchDarkLightInput.checked = false;
     logoLight.classList.add('svg-dark');
+    body.classList.add(themes.light.add.body);
+    header.classList.add(themes.light.add.header);
+    switchDarkLight.classList.add(themes.light.add.switch);
+    extensionListTitle.classList.add(themes.light.add.title);
+
+    body.classList.remove(themes.light.remove.body);
+    header.classList.remove(themes.light.remove.header);
+    switchDarkLight.classList.remove(themes.light.remove.switch);
+    extensionListTitle.classList.remove(themes.light.remove.title);
+
+    toggleClasses(toggles, themes.light.add.toggles, themes.light.remove.toggles);
+    toggleClasses(elements.cards, themes.light.add.cards, themes.light.remove.cards);
+    toggleClasses(elements.extensionName, themes.light.add.names, themes.light.remove.names);
+    toggleClasses(elements.extensionDescription, themes.light.add.descriptions, themes.light.remove.descriptions);
+    toggleClasses(elements.extensionRemoveButtons, themes.light.add.removeButtons, themes.light.remove.removeButtons);
+
     iconOfSwitch.src = 'assets/images/icon-moon.svg';
-    switchDarkLight.classList.add('switch-dark-light-when-light');
-    extensionListTitle.classList.add('h1-light');
-    toggles.forEach( el => {
-        el.classList.add('identifier-light');
-    });
-    document.querySelectorAll('.card').forEach( el => {
-        el.classList.add('card-light');
-    });
-    document.querySelectorAll('.extension-name').forEach( el => {
-        el.classList.add('h2-light')
-    });
-    document.querySelectorAll('.extension-description').forEach( el => {
-        el.classList.add('p-light');
-    });
-    document.querySelectorAll('.extension-remove').forEach( el => {
-        el.classList.add('button-remove-light');
-    });
 }
 
-export { changeToDark, changeToLight };
+function applyTransition(elements){
+    elements.forEach(el => {
+        el.classList.add('transition-class');
+    })
+}
+
+switchDarkLightLabel.addEventListener('click', () => {
+    if(switchDarkLightInput.checked){
+        changeToLight();
+    }else{
+        changeToDark();
+    }
+});
+export function changeMode(){
+    changeToDark();
+    applyTransition([
+        body,
+        header,
+        switchDarkLight,
+        extensionListTitle,
+        ...toggles,
+        ...elements.cards,
+        ...elements.extensionName,
+        ...elements.extensionDescription,
+        ...elements.extensionRemoveButtons
+    ]);
+};
+
+
+ 
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,7 @@
-import { changeToDark, changeToLight } from "../modules/stylechanges.js";
+import { changeMode } from "../modules/stylechanges.js";
 import { takeExtensionInfo } from "../modules/jsonLoader.js";
 
 document.addEventListener('DOMContentLoaded', async () => {
    await takeExtensionInfo('assets/scripts/data.json');
-   changeToDark();
+   changeMode();
 });

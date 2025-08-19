339c53dd371a5653e7df108d233200179840da56
Marcos Vinicius <avantiermv@gmail.com>
2025-08-19 20:22:03 -0300

    :recycle: Making it more editable

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,40 @@
 import { changeMode } from "../modules/stylechanges.js";
 import { takeExtensionInfo } from "../modules/jsonLoader.js";
+import { allExtensions } from "../modules/jsonLoader.js";
 
 document.addEventListener('DOMContentLoaded', async () => {
    await takeExtensionInfo('assets/scripts/data.json');
    changeMode();
+
+   const toggleButtons = document.querySelectorAll('.identifier');
+   
+   toggleButtons.forEach(toggle => {
+      const card = document.querySelectorAll('.card');
+   
+      toggle.addEventListener('click', () => {
+      const typetoggle = toggle.dataset.type;
+      if(typetoggle === 'all'){
+         card.forEach(card => {
+            card.classList.remove('hidden');
+            card.classList.add('visible');
+         });
+      }
+   
+      if(typetoggle === 'active'){
+         allExtensions.forEach(ex => {
+            if(ex.isActive){
+               console.log("Esses são os ativos: ", ex);
+            }
+         });
+      }
+   
+      if(typetoggle === 'inactive'){
+         allExtensions.forEach(ex => {
+            if(!ex.isActive){
+               console.log("Esses são os inativos: ", ex);
+            }
+         });
+      }
+      });
+   });    
 });

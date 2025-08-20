165d6479897a52a0ccca08d9cf48e04cf78feb00
Marcos Vinicius <avantiermv@gmail.com>
2025-08-20 17:50:02 -0300

    :boom: Removing unecessary actions
    I added some actions to the toggles active, inactive and all, but didn't work.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,6 +2,7 @@ import { changeMode } from "../modules/stylechanges.js";
 import { takeExtensionInfo } from "../modules/jsonLoader.js";
 import { allExtensions } from "../modules/jsonLoader.js";
 
+
 document.addEventListener('DOMContentLoaded', async () => {
    await takeExtensionInfo('assets/scripts/data.json');
    changeMode();
@@ -9,29 +10,42 @@ document.addEventListener('DOMContentLoaded', async () => {
    const toggleButtons = document.querySelectorAll('.identifier');
    
    toggleButtons.forEach(toggle => {
-      const card = document.querySelectorAll('.card');
-   
       toggle.addEventListener('click', () => {
       const typetoggle = toggle.dataset.type;
       if(typetoggle === 'all'){
-         card.forEach(card => {
+         toggle.classList.add('red-toggle-background');
+         document.querySelectorAll('.card').forEach(card => {
             card.classList.remove('hidden');
             card.classList.add('visible');
          });
       }
    
       if(typetoggle === 'active'){
-         allExtensions.forEach(ex => {
-            if(ex.isActive){
-               console.log("Esses são os ativos: ", ex);
+         allExtensions.forEach(extension => {
+            const card = document.querySelector(`.card[data-id="${extension.id}"]`);
+            if(card){
+               if(extension.isActive){
+                  card.classList.remove('hidden');
+                  card.classList.add('visible');
+               }else{
+                  card.classList.remove('visible');
+                  card.classList.add('hidden');
+               }
             }
          });
       }
    
       if(typetoggle === 'inactive'){
-         allExtensions.forEach(ex => {
-            if(!ex.isActive){
-               console.log("Esses são os inativos: ", ex);
+         allExtensions.forEach(extension => {
+            const card = document.querySelector(`.card[data-id="${extension.id}"]`);
+            if(card){
+               if(!extension.isActive){
+                  card.classList.remove('hidden');
+                  card.classList.add('visible');
+               }else{
+                  card.classList.remove('visible');
+                  card.classList.add('hidden');
+               }
             }
          });
       }
